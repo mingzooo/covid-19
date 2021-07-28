c4b0d79c0df52027f4aad851fdb3e9bece68cc8b
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-28 15:30:10 +0900

    [feat] home page(card news)

---
+++ src/Pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Style/Home.css';
+const CardNews = require('../static/CardNews.js')[0];
+const { firstCardNews, secondCardNews } = CardNews;
+function Home() {
+  return (
+    <>
+      <div className="home_body">
+        <div className="card-news">
+          {firstCardNews.map((card, id) => (
+            <img key={id} src={card} alt="img" />
+          ))}
+        </div>
+        <div className="card-news">
+          {secondCardNews.map((card, id) => (
+            <img key={id} src={card} alt="img" />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
