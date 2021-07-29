e63f9d4e5753174d2a6d6972016a37c32fca3ee6
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-29 23:27:39 +0900

    [feat] route

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import './Nav.css';
+
+export const NavBar = styled.div`
+  z-index: 999;
+  margin: 0;
+  top: 0;
+  position: fixed;
+  background-color: #ffffff;
+  border-bottom: 2px solid rgba(2, 7, 21, 0.2);
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+
+  .header__left-content,
+  .header__right-content {
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    height: 100%;
+  }
+  .header__left-content {
+    justify-content: flex-start;
+  }
+  .header__right-content {
+    justify-content: flex-end;
+  }
+  .blue {
+    background-color: #2b66ff;
+    color: white;
+  }
+`;
+
+function Nav({ domestic, local }) {
+  return (
+    <div>
+      <NavBar>
+        <div className="header__left-content">
+          <Link to="/">
+            <h1>
+              <span style={{ color: '#2B66FF' }}>Covid-19</span>
+            </h1>
+          </Link>
+          <Link to="/login">
+            <button>로그인</button>
+          </Link>
+          <Link to="/domestic">
+            <button>국내 현황</button>
+          </Link>
+          <Link to="/clinic">
+            <button>선별 진료소</button>
+          </Link>
+        </div>
+        <div className="header__right-content">
+          <Link to="/domestic">
+            <button>국내 총 확진자 수 : {domestic.TotalCase}</button>
+          </Link>
+          <Link to="/domestic">
+            <button className="blue">
+              일일 확진자 수 : {local.korea.newCase}
+            </button>
+          </Link>
+        </div>
+      </NavBar>
+    </div>
+  );
+}
+
+export default Nav;
