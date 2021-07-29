814b5c0330a01ea120a297f5086715557accc1d9
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-29 23:27:30 +0900

    [feat] home card news arr

---
+++ src/static/CardNews.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    firstCardNews: [
+      'http://ncov.mohw.go.kr/front_new/modules/img_view.jsp?img_loc=/upload/mwEditor/202107/1626080398658_20210712175958.png',
+    ],
+    secondCardNews: [
+      'http://ncov.mohw.go.kr/front_new/modules/img_view.jsp?img_loc=/upload/mwEditor/202107/1626673070465_20210719143750.jpg',
+      'http://ncov.mohw.go.kr/front_new/modules/img_view.jsp?img_loc=/upload/mwEditor/202107/1626673087826_20210719143807.jpg',
+      'http://ncov.mohw.go.kr/front_new/modules/img_view.jsp?img_loc=/upload/mwEditor/202107/1626673105435_20210719143825.jpg',
+    ],
+  },
+];
